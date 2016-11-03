0ffe41504e0d826d972a364ddebf0fdb45887b14
shigemk2 <i.am.shige@gmail.com>
2016-11-03 18:05:37 +0900

    Add case classes

--- src/main/scala/com/example/ContentEnricher.scala
+++ src/main/scala/com/example/ContentEnricher.scala
@@ -1,6 +1,26 @@
 package com.example
 
+import java.util.Date
+
 import akka.actor._
+
+case class DoctorVisitCompleted(
+                               val patientId: String,
+                               val firstName: String,
+                               val date: Date,
+                               val patientDetails: PatientDetails) {
+  def this(patientId: String, firstName: String, date: Date) = {
+    this(patientId, firstName, date, PatientDetails(null, null, null))
+  }
+
+  def carrier = patientDetails.carrier
+  def lastName = patientDetails.lastName
+  def socialSecurityNumber = patientDetails.socialSecurityNumber
+}
+
+case class PatientDetails(val lastName: String, val socialSecurityNumber: String, val carrier: String)
+
+case class VisitCompleted(dispatcher: ActorRef)
 
 object ContentEnricherDriver {
 }
